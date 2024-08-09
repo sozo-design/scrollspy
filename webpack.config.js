d14448cf3a5c585c7ca02f11ca874eb2cd6e7170
Darshan Gada <darshan@sozodesign.co.uk>
2024-08-09 21:07:38 +0530

    feat: updates

--- webpack.config.js
+++ webpack.config.js
@@ -9,6 +9,7 @@ module.exports = {
     path: path.resolve(__dirname, 'dist'),
     library: 'ScrollSpy',
     libraryTarget: 'umd',
+    libraryExport: 'default',
     globalObject: 'this'
   },
   module: {
